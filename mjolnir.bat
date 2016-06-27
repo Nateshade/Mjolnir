60453137cde69646eeb4c7720fae901807c0d761
Nateshade <totalshadowkidd@gmail.com>
2016-06-27 01:33:40 -0700

    Added Mjolnir.bat

---
+++ mjolnir.bat
@@ -0,0 +1,11 @@
+#Mjolnir_DR for Windows Batch script
+::Make screen cleaner
+echo off
+::Title
+echo Mjolnir_DR
+pause
+::The actual program
+set /p dir1="Enter name of directory to copy from(Include slashes and all): "
+set /p dir2="Enter name of directory to copy to(Include slashes and all): "
+xcopy "%dir1%" "%dir2%"
+pause

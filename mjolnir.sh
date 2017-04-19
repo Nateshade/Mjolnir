1af4de879081edf61da4a86f0dd72408021582d5
Nateshade <totalshadowkidd@gmail.com>
2017-04-19 09:05:38 -0700

    Update mjolnir.sh

--- mjolnir.sh
+++ mjolnir.sh
@@ -1,12 +1,9 @@
 #Mjolnir_DR for Linux Shell script
 #Asks for directory to copy files from:
-echo -n "Enter a directory to copy files from > "
+echo -n "Enter a directory to copy files FROM > "
 read dir
 #Asks where to copy files to:
-echo -n "What is the name of the Flash Drive or SD Card this file is running on? "
+echo -n "Enter a path to copy files TO >"
 read drive
-#Asks for username:
-echo -n "What is your username? "
-read usr
-#Copies files to current Flash Drive or SD Card:
-cp -r $dir /media/$usr/$drive
+#Copies files in dir directory to drive directory:
+cp -r $dir $drive

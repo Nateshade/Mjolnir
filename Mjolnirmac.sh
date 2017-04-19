073871af6d0244b84ac6439eaa7cd33261812554
Nateshade <totalshadowkidd@gmail.com>
2017-04-19 09:12:44 -0700

    Create Mjolnirmac.sh

---
+++ Mjolnirmac.sh
@@ -0,0 +1,10 @@
+#Mjolnir_DR for Mac Shell script
+#Asks for directory to copy files from:
+echo -n "Enter a directory to copy files FROM > "
+read dir
+#Asks where to copy files to:
+echo -n "Enter a path to copy files TO >"
+read drive
+#Copies files in dir directory to drive directory:
+echo "Your files are on their way!"
+cp -R $dir $drive
